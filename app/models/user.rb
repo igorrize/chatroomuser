7e61e9c5fe35972d97e636f1cb25a60accffda72
igorrize <igor.lobazoff@gmail.com>
2016-10-30 06:58:56 +0300

    add carrierwave uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :rooms
   has_many :messages
-
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :rooms, only: [:new, :create, :show, :index]
-
+  resources :users
   mount ActionCable.server => '/cable'
 
   root 'rooms#index'
