3c054d20069894c16ff530f8e8fae6495ea60873
igorrize <igor.lobazoff@gmail.com>
2016-11-03 11:27:03 +0200

    ###

--- db/migrate/20161027210411_create_rooms.rb
+++ db/migrate/20161027210411_create_rooms.rb
@@ -2,7 +2,7 @@ class CreateRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :rooms do |t|
       t.string :rname
-      t.references :user, foreign_key: true
+     # t.references :user, foreign_key: true
 
       t.timestamps
     end
