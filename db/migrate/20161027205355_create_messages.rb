6784991484181eea97ef1a6f564aacebdb8bc9fe
igorrize <igor.lobazoff@gmail.com>
2016-11-03 17:51:03 +0200

    wwwww

--- db/migrate/20161027205355_create_messages.rb
+++ db/migrate/20161027205355_create_messages.rb
@@ -2,7 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.string :message
-	  t.references :room, foreign_key: true
+	  t.integer :room_id, index:true
+
       t.references :user, foreign_key: true
 
       t.timestamps
