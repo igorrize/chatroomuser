88e972ff21072b30c4c91fdedf6c849b74f038b0
igorrize <igor.lobazoff@gmail.com>
2016-11-03 11:59:20 +0200

    uncoment models

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-  # belongs_to :user
-  # belongs_to :room
+   belongs_to :user
+   belongs_to :room
   validates :body, presence: true, length: { minimum: 2, maximum: 1000 }
 
   after_create_commit { MessageBroadcastJob.perform_later(self) }
